48e99f79a74e9a578b32f3def606c482cf923935
Arthur Sobreira <tutusobreira@gmail.com>
2022-10-25 10:34:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ function App() {
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type='text'
+        placeholder="Digite seu CEP..."
+        />
+      </div>
     </div>
   );
 }
